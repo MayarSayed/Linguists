4507781c52a44289a3468c93d0e578c1b644430f
Mostafa-Farghly <mostafa.alaa.16897@gmail.com>
2019-07-29 19:19:22 +0200

    Co working space profile

---
+++ app/src/main/java/com/example/go_workingspace/CwsProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.go_workingspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CwsProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cws_profile);
+    }
+}
